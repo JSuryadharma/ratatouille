6b4652d9fac5f1aa95ef88a894e625985e993a47
caturdewa <acatudewa@gmail.com>
2021-01-09 19:57:12 +0700

    Select Location
    Select Location dan Set Location udah ya

---
+++ app/src/main/java/com/example/ratatouille/SetLocation.java
@@ -0,0 +1,14 @@
+package com.example.ratatouille;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SetLocation extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_set_location);
+    }
+}
